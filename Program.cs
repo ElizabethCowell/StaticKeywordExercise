e7201e2fdb55ae675cab2a702c7171207da25b7b
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-15 12:22:00 -0400

    temp converter

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(72));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(32));
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticKeywordExercise
+{
+    class TempConverter
+    {
+        public static double FahrenheitToCelsius(double temp1)
+        {
+            var celsius = (temp1 - 32) * .5556;
+            return celsius;
+        }
+
+        public static double CelsiusToFahrenheit(double temp2)
+        {
+            var fahrenheit = temp2 * 1.8 + 32;
+            return fahrenheit;
+        }
+    }
+}
